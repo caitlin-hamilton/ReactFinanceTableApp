4839b724e30ddebffe1c6f51d7c553c762e1b56b
Caitlin Hamilton <caitlinhamilton@Caitlins-MacBook-Pro.local>
2020-09-03 09:23:27 +0100

    Styling table background colours implemented

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import Table from './Table.js'
 
 function App() {
   return (
-    <div>< Table inputRows={[{price: 100, assetClass: 'Macro', ticker: 'ALPHA'}, {price: 50, assetClass: 'Entity', ticker: 'CHARLIE'}, {price: 75, assetClass: 'Macro', ticker: 'BETA'}, {price: 175, assetClass: 'Credit', ticker: 'BETA'}]}/></div>
+    <div>< Table inputRows={[{price: 100, assetClass: 'Macro', ticker: 'ALPHA'}, {price: -50, assetClass: 'Entity', ticker: 'CHARLIE'}, {price: 50, assetClass: 'Macro', ticker: 'BETA'}, {price: 175, assetClass: 'Credit', ticker: 'BETA'}]}/></div>
     );
 }
 
--- frontend/src/InputRow.js
+++ frontend/src/InputRow.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import './App.css';
 
 export default class InputRow extends React.Component {
 
     render(){
         return(
             <tr>
-                <td>{this.props.assetClass}</td>
-                <td>{this.props.price}</td>
+                <td className={this.props.assetClass}>{this.props.assetClass}</td>
+                <td className= {this.props.price >=0 ? "PositivePrice" : "NegativePrice"}>{this.props.price}</td>
                 <td>{this.props.ticker}</td>
             </tr>
 
