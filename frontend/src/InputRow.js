b328205bd17b67f214283126d7d90b85eaf40691
Caitlin Hamilton <caitlinhamilton@Caitlins-MacBook-Pro.local>
2020-09-05 11:29:26 +0100

    functional component for inputrow

--- frontend/src/InputRow.js
+++ frontend/src/InputRow.js
@@ -2,16 +2,15 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { ASSET_CLASS_COLOURS } from './config';
 
-export default class InputRow extends React.Component {
-
-    render(){
+const InputRow = (props) => {
         return(
             <tr>
-                <td width="25%" white-space="nowrap" class={ASSET_CLASS_COLOURS[this.props.assetClass]}>{this.props.assetClass}</td>
-                <td width="25%" white-space="nowrap" class= {this.props.price >=0 ? "p-3 mb-2 bg-info text-white" : "p-3 mb-2 bg-danger text-white"}>{this.props.price}</td>
-                <td width="25%" white-space="nowrap"> {this.props.ticker}</td>
+                <td width="25%" white-space="nowrap" class={ASSET_CLASS_COLOURS[props.assetClass]}>{props.assetClass}</td>
+                <td width="25%" white-space="nowrap" class= {props.price >=0 ? "p-3 mb-2 bg-info text-white" : "p-3 mb-2 bg-danger text-white"}>{props.price}</td>
+                <td width="25%" white-space="nowrap"> {props.ticker}</td>
             </tr>
 
         )
     }
-}
+export default InputRow;
+
