355f04d4ae8b4506cd6334fd3c192a27ac1d029f
Caitlin Hamilton <caitlinhamilton@Caitlins-MacBook-Pro.local>
2020-09-05 12:10:35 +0100

    unidirectional arrow change

--- frontend/src/Table.js
+++ frontend/src/Table.js
@@ -4,12 +4,18 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import {ArrowUp, ArrowDown } from 'react-bootstrap-icons';
 import {ASSET_CLASS_SORT_ORDER} from './config';
 
-
 class Table extends React.Component {
 
     constructor(props){
         super(props);
-        this.state = {inputData: []}
+        this.state = {
+          inputData: [],
+          arrowLogic: {
+            assetClass: false,
+            price: false,
+            ticker: false,
+          }
+        };
       }
 
       componentDidMount(){
@@ -18,27 +24,36 @@ class Table extends React.Component {
           })
       }
 
+      getArrowLogic = (attribute) => {
+        let localArrowLogic = {... this.state.arrowLogic}
+        Object.keys(localArrowLogic).forEach(v => localArrowLogic[v] = false)
+        localArrowLogic[attribute] = true;
+        return localArrowLogic;
+      }
+
       sortDescendingOrder = (attribute) => {
         let data = [].concat(this.state.inputData).sort((a, b) => a[attribute] >= b[attribute] ? 1 : -1)
-        this.setState({
-            inputData: data
-        })
 
+        this.setState({
+            inputData: data,
+            arrowLogic: this.getArrowLogic(attribute),
+        })
       }
 
       sortAscendingOrder = (attribute) => {
         let data = [].concat(this.state.inputData).sort((a, b) => a[attribute] < b[attribute] ? 1 : -1)
         this.setState({
-            inputData: data
+            inputData: data,
+            arrowLogic: this.getArrowLogic(attribute),
         })
 
       }
-      sortByAssetClass = () => {
+      sortByAssetClass = (attribute) => {
         let data = [].concat(this.state.inputData).sort((a, b) => ASSET_CLASS_SORT_ORDER[a.assetClass] - ASSET_CLASS_SORT_ORDER[b.assetClass])
         this.setState({
-          inputData: data
+          inputData: data,
+          arrowLogic: this.getArrowLogic(attribute),
       })
-
     }
 
     render() {
@@ -48,9 +63,9 @@ class Table extends React.Component {
               <div >
                 <table class="table table-bordered" >
                     <tr>
-                        <th onClick={() => {this.sortByAssetClass()}}> Asset Class <ArrowDown /></th>
-                        <th onClick={() => {this.sortAscendingOrder('price')}}>Price <ArrowUp /></th>
-                        <th onClick={() => {this.sortDescendingOrder('ticker')}}>Ticker <ArrowDown /></th>
+                        <th onClick={() => {this.sortByAssetClass('assetClass')}}> Asset Class {this.state.arrowLogic['assetClass'] ? <ArrowDown/> : null}</th>
+                        <th onClick={() => {this.sortAscendingOrder('price')}}>Price {this.state.arrowLogic['price'] ? <ArrowUp />: null}</th>
+                        <th onClick={() => {this.sortDescendingOrder('ticker')}}>Ticker {this.state.arrowLogic['ticker'] ? <ArrowDown />: null }</th>
                     </tr>
                    <tbody>
                    {this.state.inputData.map((item, index) => <InputRow assetClass={item.assetClass} price={item.price} ticker={item.ticker} key={index}/>)}
