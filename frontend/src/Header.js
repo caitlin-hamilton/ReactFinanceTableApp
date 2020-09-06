d5f2752c54db918faf558c1eb0f79db335305178
Caitlin Hamilton <caitlinhamilton@caitlins-mbp.mynet>
2020-09-06 12:42:36 +0100

    added header function

---
+++ frontend/src/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {OverlayTrigger, Tooltip} from 'react-bootstrap';
+import {ArrowDown, ArrowUp } from 'react-bootstrap-icons';
+import {SORT_ARROW} from './config';
+
+const HeaderSpecialColumn = (props) => (
+    <OverlayTrigger
+    placement="bottom"
+    delay={{show: 50, hide: 100}}
+overlay={<Tooltip>Click to sort this column {props.sortIcon == 'DOWN' ? <ArrowUp /> : <ArrowDown />}</Tooltip>}
+    >
+    <th onClick={() => {props.sortAttribute(props.attribute)}}>{props.attribute} {SORT_ARROW[props.sortIcon]}</th>
+    </OverlayTrigger>
+);
+
+  export default HeaderSpecialColumn
